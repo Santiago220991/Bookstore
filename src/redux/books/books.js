f1a501e6c2dbb93b3566a086852b3727a3b305b7
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-06 16:29:01 -0500

    Update constans ADD and REMOVE to follow dukcs pattern

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,15 +1,15 @@
-const ADD = 'Bookstore/books/ADD';
-const REMOVE = 'Bookstore/books/REMOVE';
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
 const Book = (state, action) => {
   switch (action.type) {
-    case ADD:
+    case ADD_BOOK:
       return [...state, {
         id: state.length,
         title: action.payload.title,
         author: action.payload.author,
       }];
-    case REMOVE:
+    case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
     default:
       return state;
@@ -17,12 +17,12 @@ const Book = (state, action) => {
 };
 
 export const AddBook = (title, author) => ({
-  type: ADD,
+  type: ADD_BOOK,
   payload: { title, author },
 });
 
 export const RemoveBook = (id) => ({
-  type: REMOVE,
+  type: REMOVE_BOOK,
   payload: id,
 });
 
