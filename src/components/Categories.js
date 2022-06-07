214836c56eda8c97fcb4dc76ebc2c85e6c55d355
Alexander Santiago Cárdenas Padilla <alexander.cardenas10@outlook.com>
2022-06-07 11:30:12 -0500

    Add message in categories pages using redux

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Categories.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { building } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <button className="categoriesbtn" type="button">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const message = useSelector((state) => state.Check);
+  const dispatch = useDispatch();
 
+  const showmessage = () => {
+    dispatch(building());
+  };
+  return (
+    <div>
+      <button className="categoriesbtn" type="button" onClick={showmessage}>Check Status</button>
+      <div id="message">{message}</div>
+    </div>
+  );
+};
 export default Categories;
